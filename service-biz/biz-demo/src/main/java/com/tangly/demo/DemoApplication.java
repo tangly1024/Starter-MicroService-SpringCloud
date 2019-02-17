1ee6a912526c8b78d74e8fd03b7fbc7bbce25b0c
tangly <tlyong1992@hotmail.com>
2019-02-17 22:16:55 +0800

    加入ZUUL网关

--- service-biz/biz-demo/src/main/java/com/tangly/demo/DemoApplication.java
+++ service-biz/biz-demo/src/main/java/com/tangly/demo/DemoApplication.java
@@ -3,13 +3,11 @@ package com.tangly.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.core.env.Environment;
 
 @SpringCloudApplication
-@EnableEurekaClient
 @EnableFeignClients
 @EnableHystrixDashboard
 public class DemoApplication {
