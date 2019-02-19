68a9b19b212c83215e5de8356f82f9e7646bff51
573865178 <573865178@qq.com>
2019-02-19 21:11:01 +0800

    基于rabbitmq实现zipkin-sleuth

--- zipkin-server/src/main/java/com/tangly/zipkin/ZipkinApplication.java
+++ zipkin-server/src/main/java/com/tangly/zipkin/ZipkinApplication.java
@@ -2,9 +2,9 @@ package com.tangly.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin.server.EnableZipkinServer;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
-@EnableZipkinServer
+@EnableZipkinStreamServer
 @SpringBootApplication
 public class ZipkinApplication {
 
